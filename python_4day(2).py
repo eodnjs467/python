c34b1312314f7e2e5d2a466ae3e1950742a79fea
eodnjs467 <sponjjanc@naver.com>
2019-12-25 15:41:14 +0900

    python study 5days

--- python_4day(2).py
+++ python_4day(2).py
@@ -1,2 +1,68 @@
-##배열(Array)
+#제어문
 
+##  if 조건문:
+##      수행할 문장1
+##      수행할 문장2
+##  else :
+##      수행할 문자a
+##      수행할 문자b
+
+## 3000원 이상의 돈을 가지고 있으면 택시를 타고 그렇지 않으면 걸어가라.
+money = 2000
+
+if money>=3000 :
+    print('택시를 타라')
+else :
+    print('걸어가라')
+
+## 3000원 이상 있거나 카드가 있다면 택시를 타고 그렇지 않으면 걸어가라.
+money = 2000
+card = True
+
+if money>=3000 or card :
+    print('택시타라')
+else:
+    print('걸어가라')
+
+## 주머니에 돈이 있으면 택시를 타고, 없으면 걸어가라.
+pocket = ['paper', 'cellphone', 'money']
+
+if 'money' in pocket:
+    print('택시타라')
+else:
+    print('걸어가라')
+
+
+#### 주머니에 돈이 있으면 가만히 있고, 돈이 없으면 카드를 꺼내라.
+pocket = ['paper', 'money', 'cellphone']
+if 'money' in pocket:
+    pass
+else:
+    print('카드를 꺼내라')
+
+####주머니에 돈이 있으면 택시를 타고,
+# 주머니에 돈은 없지만 카드가 있으면 택시를 타고, 돈도 없고 카드도 없으면 걸어 가라
+
+pocket = ['paper', 'handphone']
+card = True
+if 'money' in pocket:
+    print('택시타라')
+elif card:
+    print('택시타라')
+else:
+    print('걸어가라')
+
+if 'money' in pocket: pass
+else: print('카드를 꺼내라')
+
+
+## 조건부 표현식( 조건문이 참인 경우  if 조건문 else 조건문이 거짓인 경우)
+score = 76
+if score >=60:
+    message = "success"
+else:
+    message = "failure"
+print(message)
+
+message = 'success' if score>=60 else 'failure'
+print(message)
---
+++ python_5day.py
@@ -0,0 +1,45 @@
+##배열(Array)
+
+#1차원 배열 : 리스트로 구현시
+data = [1, 2, 3, 4, 5]
+print(data)
+#2차원 배열 : 리스트로 구현시
+data = [[1, 2, 3], [4, 5, 6], [7,8, 9]]
+print(data[0][1])
+print(data[1][1])
+#### 연습1 : 위의 2차원 배열에서 9,8,7,순서로 출력해보기
+print(data[2][2], data[2][1], data[2][0])
+#### 연습2 : 다음 dataset 에서 전체 이름 안에 M이 몇 번 나왔는지 빈도수 출력
+dataset = ['Braund, Mr. Owen Harris',
+           'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',
+           'Heikkinen, Miss. Laina',
+           'Futrelle, Mrs. Jacques Heath (Lily May Peel)',
+           'Allen, Mr. William Henry',
+           'Moran, Mr. James',
+           'McCarthy, Mr. Timothy J',
+           'Palsson, Master. Gosta Leonard',
+           'Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)',
+           'Nasser, Mrs. Nicholas (Adele Achem)',
+           'Sandstrom, Miss. Marguerite Rut',
+           'Bonnell, Miss. Elizabeth',
+           'Saundercock, Mr. William Henry',
+           'Andersson, Mr. Anders Johan',
+           'Vestrom, Miss. Hulda Amanda Adolfina',
+           'Hewlett, Mrs. (Mary D Kingcome) ',
+           'Rice, Master. Eugene',
+           'Williams, Mr. Charles Eugene',
+           'Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)',
+           'Masselmani, Mrs. Fatima',
+           'Fynney, Mr. Joseph J',
+           'Beesley, Mr. Lawrence',
+           'McGowan, Miss. Anna \"Annie\"',
+           'Sloper, Mr. William Thompson',
+           'Palsson, Miss. Torborg Danira',
+           'Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)',
+           'Emir, Mr. Farred Chehab',
+           'Fortune, Mr. Charles Alexander',
+           'Dwyer, Miss. Ellen \"Nellie\"',
+           'Todoroff, Mr. Lalio']
+m_count = 0
+for data in dataset :
+    print(data)
