df0a69fe369673d35f053748a1a7e7e89c9b891c
eodnjs467 <sponjjanc@naver.com>
2019-12-31 23:03:12 +0900

    python study 8days

--- python_9day.py
+++ python_9day.py
@@ -12,3 +12,19 @@ def getTotalPage(m, n):
     return total
 a = getTotalPage(5, 10)
 int(a)
+
+## 간단한 메모장 만들기
+import sys
+option = sys.argv[1]
+
+if option == '-a':
+    memo = sys.argv[2]
+    f = open('memo.txt', 'a')
+    f.write(memo)
+    f.write('\n')
+    f.close()
+elif option == '-v':
+    f = open('memo.txt')
+    memo = f.read()
+    f.close()
+    print(memo)
